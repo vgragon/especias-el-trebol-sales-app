4974ecab500eeaf8510bd945d73b3af6f2bfd056
Vicente Grajeda <vicente.grajeda@gmail.com>
2017-08-18 14:05:29 -0500

    Use found stackoverflow function to format currency

--- src/app/components/sales/SalesService.js
+++ src/app/components/sales/SalesService.js
@@ -17,15 +17,14 @@ export default {
     },
     formatNumber: function (criteria, number) {
         if (criteria === "CURRENCY") {
-            let numberStringed = (number + "").split("").reverse().join("");
-            let formattedNumber = "";
-            let counter = 1;
-            for (let char of numberStringed) {
-                if (counter % 4 === 0 && !Number.isNaN(Number.parseInt(char))) formattedNumber = "," + formattedNumber;
-                formattedNumber = char + formattedNumber;
-                counter++;
-            }
-            return formattedNumber;
+            let n = number,
+                c = isNaN(c = Math.abs(c)) ? 2 : c,
+                d = d === undefined ? "." : d,
+                t = t === undefined ? "," : t,
+                s = n < 0 ? "-" : "",
+                i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
+                j = (j = i.length) > 3 ? j % 3 : 0;
+            return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
         }
     },
     toISOString: function (dt) {
